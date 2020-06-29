9e5fdbd0a2fecc77a167f3eb57a2fcd5bc7d0a2b
2dubbing <2dubbing@gmail.com>
2020-06-29 21:05:01 +0900

    Remove window.error function in index.js

--- public/js/index.js
+++ public/js/index.js
@@ -2,8 +2,8 @@ const MAIN_SERVER_URL = 'http://localhost:3000';
 const STATIC_SERVER_URL = 'http://localhost:3001';
 
 window.onload = function() {
+  const img = document.querySelector('.content--image > img');
   const setImage = (imgSrc) => {
-    const img = document.querySelector('.content--image > img');
     img.src = imgSrc;
     img.alt = '이미지';
   }
@@ -29,27 +29,4 @@ window.onload = function() {
       setImage(images[0]);
     });
   });  
-};
-
-window.onerror = function (msg, url, lineNo, columnNo, error) {
-  const logBox = document.querySelector('.content--section__log');
-  const string = msg.toLowerCase();
-  const substring = "script error";
-
-  if (string.indexOf(substring) > -1){
-    logBox.value = 'Script Error: See Browser Console for Detail';
-
-  } else {
-    const message = [
-      'Message: ' + msg,
-      'URL: ' + url,
-      'Line: ' + lineNo,
-      'Column: ' + columnNo,
-      'Error object: ' + JSON.stringify(error)
-    ].join(' - ');
-
-    logBox.value = message;
-  }
-
-  return false;
 };
